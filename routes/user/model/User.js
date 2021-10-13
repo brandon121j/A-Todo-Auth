30d6b9518754b867adb0cf0afb48672472ea1201
Brandon <brandon121j@gmail.com>
2021-10-13 02:09:07 -0400

    completion of user model

---
+++ routes/user/model/User.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema (
+    {
+        firstName: {
+            type: String
+        },
+        lastName: {
+            type: String
+        },
+        username: {
+            type: String,
+            unique: true
+        },
+        email: {
+            type: String,
+            unique: true
+        },
+        password: {
+            type: String,
+        },
+        todos: [{ type: mongoose.Schema.ObjectId, ref: "todo" }]
+    },
+    {
+        timestamps: true
+    }
+)
+
+module.exports = mongoose.model('user', userSchema);
