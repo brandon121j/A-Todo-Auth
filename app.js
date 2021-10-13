8b06b1031e8c38cc95c806ddcabafd96f1d5a674
Brandon <brandon121j@gmail.com>
2021-10-13 02:01:29 -0400

    Changes made to app.js file

--- app.js
+++ app.js
@@ -3,9 +3,18 @@ var createError = require('http-errors');
 var express = require('express');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose')
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var userRouter = require('./routes/user/userRouter');
+var todoRouter = require('./routes/todo/todoRouter');
+
+mongoose.connect(process.env.MONGO_DB)
+  .then(() => {
+    console.log("MONGODB CONNECTED")
+  })
+  .catch((error) => {
+    console.log(error)
+  })
 
 var app = express();
 
@@ -14,8 +23,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api/user', userRouter);
+app.use('/api/todo', todoRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
