c1c96dd7a4343cc6d31f440dbef132d97311a23d
Brandon <brandon121j@gmail.com>
2021-10-13 12:06:59 -0400

    Addition of login function

--- routes/user/controller/userController.js
+++ routes/user/controller/userController.js
@@ -27,6 +27,73 @@ async function register (req, res) {
     }
 }
 
+//Added ability to login with either username or password
+async function login (req, res) {
+    const { username, password, email } = req.body
+
+    try {
+        
+        let foundUser = await User.findOne({ username: username })
+
+        if (!foundUser) {
+            let foundEmail = await User.findOne({ email: email });
+
+            if (!foundUser && !foundEmail) {
+                res.status(500).json({
+                    message: "ERROR",
+                    error: "Invalid login credentials"
+                })
+            } else {
+                let comparedPassword = bcrypt.compare(password, foundEmail.password);
+            
+                if (!comparedPassword) {
+                    res.status(500).json({
+                        messsage: "ERROR",
+                        error: "Please check your email and password"
+                    })
+                } else {
+                    let jwtToken = jwt.sign(
+                        {
+                            email: foundEmail.email,
+                            username: foundEmail.username
+                        },
+                        process.env.JWT_SECRET,
+                        { expiresIn: "24h" }
+                    );
+
+                    res.json({ message: "SUCCESS", payload: jwtToken });
+                }
+            
+            }
+        } else {
+            let passwordComparer = await bcrypt.compare (password, foundUser.password);
+        
+            if (!passwordComparer) {
+                return res.status(500).json({
+                    message: "ERROR",
+                    error: "Please check username and password"
+                })
+            } else {
+                let jwtToke = jwt.sign(
+                    {
+                        email: foundUser.email,
+                        username: foundUser.username
+                    },
+                    process.env.JWT_SECRET,
+                    { expiresIn: "24h" }
+                );
+                res.json({ message: "SUCCESS", payload: jwtToke })
+            }
+        }
+    } catch(error) {
+        res.status(500).json({
+            message: "ERROR",
+            error: error.message
+        })
+    }
+}
+
 module.exports = {
-    register
+    register,
+    login
 }
--- routes/user/userRouter.js
+++ routes/user/userRouter.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var router = express.Router();
-const { register } = require('./controller/userController');
+const { register, login } = require('./controller/userController');
 
 router.post('/create-user', register);
+
+router.post('/login', login);
 
 module.exports = router
